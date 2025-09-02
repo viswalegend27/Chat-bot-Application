f43145918dc485c82d733660d0c396809745d9ba
Viswa V <viswamalar2004@gmail.com>
2025-09-02 16:16:42 +0530

    Update settings.py and add requirements for Render deployment

--- chatbot_project/settings.py
+++ chatbot_project/settings.py
@@ -29,6 +29,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware", 
 ]
 
 ROOT_URLCONF = 'chatbot_project.urls'
